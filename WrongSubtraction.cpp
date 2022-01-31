20f45f26c067792db9bbe1dd249c765d43382f20
Nguyễn Lê Đông Mạnh <98057363+nguyenledongmanh@users.noreply.github.com>
2022-02-01 02:29:30 +0700

    Bài số 14

---
+++ WrongSubtraction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long int n;
+    int k;
+    cin >> n >> k;
+
+    while (k--)
+    {
+        if (n % 10 != 0)
+            n--;
+        else
+            n /= 10;
+    }
+
+    cout << n << " " << endl;
+
+    return 0;
+}
